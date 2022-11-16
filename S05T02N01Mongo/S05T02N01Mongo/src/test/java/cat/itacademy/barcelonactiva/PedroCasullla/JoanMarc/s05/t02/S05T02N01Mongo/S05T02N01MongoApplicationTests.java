7558a83b65be0099e1a2bca960b89e1864b1241e
Joan Marc <jmpcasulla@gmail.com>
2022-11-16 10:21:36 +0100

    addMongo

---
+++ S05T02N01Mongo/S05T02N01Mongo/src/test/java/cat/itacademy/barcelonactiva/PedroCasullla/JoanMarc/s05/t02/S05T02N01Mongo/S05T02N01MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.PedroCasullla.JoanMarc.s05.t02.S05T02N01Mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T02N01MongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
